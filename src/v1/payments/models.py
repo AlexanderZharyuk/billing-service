83b5add5f60ba0e6124dd86574faad46447e73f3
VyacheslavKazakov <44771742+VyacheslavKazakov@users.noreply.github.com>
2024-03-01 11:16:51 +0200

    [diploma][task #2] models corrected 0.1 (#20)
    * [diploma][task #2] models corrected 0.1
    
    * [diploma][task #2] models corrected 0.2
    
    * [diploma][task #2] models corrected 0.3
    
    ---------
    
    Co-authored-by: Казаков Вячеслав <vyachkaathome@yandex.ru>

--- src/v1/payments/models.py
+++ src/v1/payments/models.py
@@ -1,12 +1,19 @@
 from decimal import Decimal
 from enum import Enum
-from typing import Optional
+from typing import Optional, TYPE_CHECKING
+from uuid import UUID
 
-from sqlmodel import SQLModel, Field, Relationship, CheckConstraint
+from sqlmodel import CheckConstraint
+from sqlmodel import SQLModel, Field, Relationship
 
 from src.models import BaseResponseBody, Base
 from src.models import TimeStampedMixin
 from src.v1.payment_providers.models import PaymentProvider
+from src.v1.plans import Plan
+
+if TYPE_CHECKING:
+    from src.v1.subscriptions.models import Subscription
+    from src.v1.payment_providers.models import PaymentProvider
 
 
 class PaymentStatusEnum(str, Enum):
@@ -18,9 +25,17 @@ class PaymentStatusEnum(str, Enum):
 
 
 class CurrencyEnum(str, Enum):
-    RUB = "rub"
-    USD = "usd"
-    EUR = "eur"
+    RUB = "RUB"
+    USD = "USD"
+    EUR = "EUR"
+
+
+class PaymentMethodsEnum(str, Enum):
+    BANK_CARD = "bank_card"
+    YOO_MONEY = "yoo_money"
+    SBERBANK = "sberbank"
+    TINKOFF_BANK = "tinkoff_bank"
+    SBP = "sbp"
 
 
 class Payment(Base, TimeStampedMixin, table=True):
@@ -34,33 +49,45 @@ class Payment(Base, TimeStampedMixin, table=True):
         primary_key=True,
         schema_extra={"examples": [5]},
     )
-    subscription_id: int = Field(foreign_key="subscriptions.id")
     subscription: "Subscription" = Relationship(back_populates="payments")
     payment_provider_id: int = Field(foreign_key="payment_providers.id")
     payment_provider: "PaymentProvider" = Relationship(back_populates="payments")
-    actual_payment_id: str = Field(default=None, max_length=50)
-    status: PaymentStatusEnum = Field(default=PaymentStatusEnum.CREATED,)
-    currency: CurrencyEnum = Field(default=CurrencyEnum.RUB,)
+    payment_type: PaymentStatusEnum = Field(
+        default=PaymentMethodsEnum.BANK_CARD,
+    )
+    status: PaymentStatusEnum = Field(
+        default=PaymentStatusEnum.CREATED,
+    )
+    currency: CurrencyEnum = Field(
+        default=CurrencyEnum.RUB,
+    )
     amount: Decimal = Field(max_digits=8, decimal_places=2)
+    external_payment_id: Optional[str] = Field(default=None, max_length=50)
+    external_payment_type_id: Optional[str] = Field(default=None, max_length=50)
 
     def __repr__(self) -> str:
         return f"Payment(id={self.id!r}, status={self.status!r}, amount={self.amount!r}, currency={self.currency!r})"
 
 
 class PaymentCreate(SQLModel):
-    name: str
-    subscription_id: int
+    plan: Plan
     payment_provider_id: int
-    status: PaymentStatusEnum
+    payment_method: PaymentMethodsEnum
     currency: CurrencyEnum
     amount: Decimal
 
 
 class PaymentUpdate(SQLModel):
-    name: Optional[str] = Field(default=None)
     status: Optional[PaymentStatusEnum] = Field(default=None)
-    currency: Optional[CurrencyEnum] = Field(default=None)
-    amount: Optional[Decimal] = Field(default=None)
+    external_payment_id: Optional[str] = Field(default=None)
+    external_payment_type_id: Optional[str] = Field(default=None)
+
+
+class PaymentMetadata(SQLModel):
+    subscription_id: int
+    payment_provider_id: int
+    user_id: UUID
+    plan_id: int
 
 
 class SinglePaymentResponse(BaseResponseBody):
