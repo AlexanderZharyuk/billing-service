3c1ba987abef5ebc71bac833cb41d4091bbbf9f0
Alexandr Jariuc <alexander.zharyuk@gmail.com>
2024-03-06 20:40:31 +0300

    refactor: Remove unique constraint from external payment id

--- src/v1/payments/models.py
+++ src/v1/payments/models.py
@@ -63,7 +63,7 @@ class Payment(Base, TimeStampedMixin, table=True):
         sa_column=Column(SQLModelEnum(CurrencyEnum)),
     )
     amount: Decimal = Field(max_digits=8, decimal_places=2)
-    external_payment_id: Optional[str] = Field(default=None, max_length=50, unique=True)
+    external_payment_id: Optional[str] = Field(default=None, max_length=50)
 
     def __repr__(self) -> str:
         return f"Payment(id ={self.id!r}, status={self.status!r}, amount={self.amount!r}, currency={self.currency!r})"
